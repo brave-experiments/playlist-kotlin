65bf3cd8eab6cda35f2675d4d580331f836e54a1
Vinod Kumar <vinodghai713@gmail.com>
2022-07-12 10:44:47 -0400

    Playlist UI XML is created

---
+++ src/main/java/com/brave/braveandroidplaylist/view/PlaylistOptionsBottomSheet.kt
@@ -0,0 +1,38 @@
+package com.brave.braveandroidplaylist.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.brave.braveandroidplaylist.R
+import com.brave.braveandroidplaylist.adapter.PlaylistOptionsBottomSheetAdapter
+import com.brave.braveandroidplaylist.model.PlaylistOptionsModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class PlaylistOptionsBottomSheet(private val optionsList: MutableList<PlaylistOptionsModel>) :
+    BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.bottom_sheet_playlist_options, container, false)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val rvBottomSheet: RecyclerView = view.findViewById(R.id.rvBottomSheet)
+        rvBottomSheet.layoutManager = LinearLayoutManager(view.context)
+        rvBottomSheet.adapter = PlaylistOptionsBottomSheetAdapter(optionsList)
+
+        val behavior = BottomSheetBehavior.from(view.findViewById(R.id.layoutBottomSheet))
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        behavior.isDraggable = false
+
+        dialog?.window?.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+            ?.setBackgroundResource(android.R.color.transparent)
+    }
+}
