b0775cac14d408cf367e6a4e47f0747a750d2a79
Vinod Kumar <vinodghai713@gmail.com>
2022-07-18 14:55:30 -0400

    BottomSheet UI Fixes

--- src/main/java/com/brave/braveandroidplaylist/view/AddOrMoveToPlaylistBottomSheet.kt
+++ src/main/java/com/brave/braveandroidplaylist/view/AddOrMoveToPlaylistBottomSheet.kt
@@ -8,9 +8,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.brave.braveandroidplaylist.R
 import com.brave.braveandroidplaylist.adapter.PlaylistAdapter
+import com.brave.braveandroidplaylist.extension.setTopCornersRounded
 import com.brave.braveandroidplaylist.model.PlaylistModel
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.card.MaterialCardView
 
 class AddOrMoveToPlaylistBottomSheet(private val playlists: MutableList<PlaylistModel>) :
     BottomSheetDialogFragment() {
@@ -24,11 +26,14 @@ class AddOrMoveToPlaylistBottomSheet(private val playlists: MutableList<Playlist
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val layoutBottomSheet: MaterialCardView = view.findViewById(R.id.layoutBottomSheet)
+        layoutBottomSheet.setTopCornersRounded(16)
+
         val rvPlaylists: RecyclerView = view.findViewById(R.id.rvPlaylists)
         rvPlaylists.layoutManager = LinearLayoutManager(view.context)
         rvPlaylists.adapter = PlaylistAdapter(playlists)
 
-        val behavior = BottomSheetBehavior.from(view.findViewById(R.id.layoutBottomSheet))
+        val behavior = BottomSheetBehavior.from(layoutBottomSheet)
         behavior.state = BottomSheetBehavior.STATE_EXPANDED
         behavior.isDraggable = false
 
--- src/main/java/com/brave/braveandroidplaylist/view/PlaylistOptionsBottomSheet.kt
+++ src/main/java/com/brave/braveandroidplaylist/view/PlaylistOptionsBottomSheet.kt
@@ -8,9 +8,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.brave.braveandroidplaylist.R
 import com.brave.braveandroidplaylist.adapter.PlaylistOptionsBottomSheetAdapter
+import com.brave.braveandroidplaylist.extension.setTopCornersRounded
 import com.brave.braveandroidplaylist.model.PlaylistOptionsModel
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.card.MaterialCardView
 
 class PlaylistOptionsBottomSheet(private val optionsList: MutableList<PlaylistOptionsModel>) :
     BottomSheetDialogFragment() {
@@ -24,11 +26,15 @@ class PlaylistOptionsBottomSheet(private val optionsList: MutableList<PlaylistOp
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val layoutBottomSheet: MaterialCardView = view.findViewById(R.id.layoutBottomSheet)
+        layoutBottomSheet.setTopCornersRounded(16)
+
         val rvBottomSheet: RecyclerView = view.findViewById(R.id.rvBottomSheet)
         rvBottomSheet.layoutManager = LinearLayoutManager(view.context)
         rvBottomSheet.adapter = PlaylistOptionsBottomSheetAdapter(optionsList)
 
-        val behavior = BottomSheetBehavior.from(view.findViewById(R.id.layoutBottomSheet))
+        val behavior = BottomSheetBehavior.from(layoutBottomSheet)
         behavior.state = BottomSheetBehavior.STATE_EXPANDED
         behavior.isDraggable = false
 
